7d162463b56575310bf727cfb702a1694a6819e1
Salil Wadnerkar <bsdprg@icloud.com>
2024-10-05 20:27:32 -0700

    simplified the date time display

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,30 @@
 use std::env;
 use std::fs;
 use std::thread;
-use std::time::{Duration, Instant};
+use std::time::Duration;
+use chrono::{DateTime, Local};
 use std::path::PathBuf;
 
 
-const POLL_DURATION: Duration = Duration::from_secs(1);
-const BATT_UPDATE_INTERVAL: u64 = 30;
+const POLL_DURATION: Duration = Duration::from_secs(60);
 
 fn main() {
     let battery: String = env::args().nth(1).expect("battery ID not provided");
+    let path = format!("/sys/class/power_supply/{}", battery);
+    let path = PathBuf::from(path);
 
-    let mut battery_info = update_battery_info(&battery);
-
-    let start = Instant::now();
     loop {
-        let duration = start.elapsed().as_secs();
-        if duration % BATT_UPDATE_INTERVAL == 0 {
-            battery_info = update_battery_info(&battery);
-        }
-        let datetime = chrono::Local::now().to_rfc2822();
+        let battery_info = update_battery_info(&path);
+        let current_local: DateTime<Local> = Local::now();
+        let datetime = current_local.format("%a,%v %H:%M");
 
-        println!("{0} | {1}", battery_info, datetime);
+        println!(" {} | {}", battery_info, datetime);
 
         thread::sleep(POLL_DURATION);
     }
 }
 
-fn update_battery_info(battery: &String) -> String {
-    let mut path = PathBuf::from("/sys/class/power_supply");
-    path.push(battery);
+fn update_battery_info(path: &PathBuf) -> String {
     let status = fs::read_to_string(path.join("status")).expect("Failed to read battery status");
     let status = status.trim();
     let charge_full = fs::read_to_string(path.join("charge_full")).expect("Failed to read battery charge_full");
