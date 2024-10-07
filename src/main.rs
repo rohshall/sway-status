5af1f3aa6393d6d0001b4c9027210a3ec999a7c6
Salil Wadnerkar <bsdprg@icloud.com>
2024-10-06 19:24:19 -0700

    Added nerd symbols for battery

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,44 @@ fn main() {
     let path = PathBuf::from(path);
 
     loop {
-        let battery_info = update_battery_info(&path);
         let current_local: DateTime<Local> = Local::now();
         let datetime = current_local.format("%a,%v %H:%M");
+        let battery_status = get_battery_status(&path);
+        let battery_charge_percentage = get_battery_charge_percentage(&path);
+        let battery_status = match battery_status.as_str() {
+            "full" => "󰂅",
+            "charging" => match battery_charge_percentage {
+                0..33 => "󰂆",
+                33..66 => "󱊥",
+                66..99 => "󱊦",
+                _ => "󰂑"
+            },
+            "discharging" => match battery_charge_percentage {
+                0..33 => "󱊡",
+                33..66 => "󱊢",
+                66..99 => "󱊣",
+                _ => "󰂑"
+            },
+            _ => "󰂑"
+        };
 
-        println!("{} | {}", battery_info, datetime);
+        println!("{} {}%  {}", battery_status, battery_charge_percentage, datetime);
 
         thread::sleep(POLL_DURATION);
     }
 }
 
-fn update_battery_info(path: &PathBuf) -> String {
-    let status = fs::read_to_string(path.join("status")).expect("Failed to read battery status");
-    let status = status.trim();
+fn get_battery_status(path: &PathBuf) -> String {
+    let battery_status = fs::read_to_string(path.join("status")).expect("Failed to read battery status");
+    String::from(battery_status.trim().to_lowercase())
+}
+
+fn get_battery_charge_percentage(path: &PathBuf) -> u64 {
     let charge_full = fs::read_to_string(path.join("charge_full")).expect("Failed to read battery charge_full");
     let charge_now = fs::read_to_string(path.join("charge_now")).expect("Failed to read battery charge_now");
     let charge_full: u64 = charge_full.trim().parse().expect("Not a valid charge_full value");
     let charge_now: u64 = charge_now.trim().parse().expect("Not a valid charge_now value");
-    let charge_percentage = (charge_now * 100) / charge_full;
-
-    return format!("{}: {}%", status, charge_percentage);
+    (charge_now * 100) / charge_full
 }
 
 
