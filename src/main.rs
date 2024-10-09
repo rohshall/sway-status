b7ac70e6c7e6e3205e3695841f34ee82828f16d2
Salil Wadnerkar <bsdprg@icloud.com>
2024-10-08 20:57:08 -0700

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,57 @@ use chrono::{DateTime, Local};
 use std::path::PathBuf;
 
 
-const POLL_DURATION: Duration = Duration::from_secs(10);
+const POLL_DURATION: Duration = Duration::from_secs(5);
 
 fn main() {
     let battery_id = env::args().nth(1).unwrap_or(String::from(""));
     loop {
-        let current_local: DateTime<Local> = Local::now();
-        let datetime = current_local.format("%a,%v %H:%M");
-
-        let mem_info = get_mem_info();
-        let load_avg = get_load_avg();
+        print_load_avg();
+        print_mem_info();
         // If battery ID is provided, show its status.
-        if battery_id.is_empty() {
-            println!("󰻠  {:.2} {:.2} {:.2}  󰍛 {}/{}({}) GB     {}", load_avg[0], load_avg[1], load_avg[2], mem_info.get("MemAvailable").unwrap(), mem_info.get("MemTotal").unwrap(), mem_info.get("MemFree").unwrap(), datetime);
-        } else {
-            let (battery_status, battery_charge_percentage) = get_battery_info(&battery_id);
-            println!("󰻠  {:.2} {:.2} {:.2}  󰍛 {}/{}({}) GB   {} {}%     {}", load_avg[0], load_avg[1], load_avg[2], mem_info.get("MemAvailable").unwrap(), mem_info.get("MemTotal").unwrap(), mem_info.get("MemFree").unwrap(), battery_status, battery_charge_percentage, datetime);
+        if !battery_id.is_empty() {
+            print_battery_info(&battery_id);
         }
+        print_datetime();
+        // Indicate the end of line
+        println!();
         // sleep
         thread::sleep(POLL_DURATION);
     }
 }
 
-fn get_battery_info(battery: &str) -> (String, u64) {
+fn print_load_avg() {
+    let contents = std::fs::read_to_string("/proc/loadavg").expect("Could not read /proc/loadavg");
+    let load_avg = contents.trim().split(' ')
+        .take(3)
+        .map(|val| val.parse::<f64>().unwrap())
+        .collect::<Vec<f64>>();
+    print!("󰻠  {:.2} {:.2} {:.2} ", load_avg[0], load_avg[1], load_avg[2]);
+}
+
+fn print_mem_info() {
+    let contents = std::fs::read_to_string("/proc/meminfo").expect("Could not read /proc/meminfo");
+    let mut mem_info = HashMap::new();
+    for line in contents.lines().take(3) {
+        let mut split_line = line.split_whitespace();
+        let label = split_line.next();
+        let value = split_line.next();
+        if value.is_some() && label.is_some() {
+            let label = label.unwrap().split(':').nth(0).unwrap();
+            let value = value.unwrap().parse::<u64>().ok().unwrap();
+            mem_info.insert(String::from(label), value/1000000);
+        }
+    }
+    print!("󰍛 {}/{}({}) GB ", mem_info.get("MemAvailable").unwrap(), mem_info.get("MemTotal").unwrap(), mem_info.get("MemFree").unwrap());
+}
+
+fn print_datetime() {
+    let current_local: DateTime<Local> = Local::now();
+    let datetime = current_local.format("%a,%v %H:%M");
+    print!("  {} ", datetime);
+}
+
+fn print_battery_info(battery: &str) {
     let path = format!("/sys/class/power_supply/{}", battery);
     let path = PathBuf::from(path);
     let charge_full = fs::read_to_string(path.join("charge_full")).expect("Failed to read battery charge_full");
@@ -55,30 +83,6 @@ fn get_battery_info(battery: &str) -> (String, u64) {
         },
         _ => "󰂑"
     };
-    (String::from(battery_status), battery_charge_percentage)
-}
-
-fn get_mem_info() -> HashMap<String, u64> {
-    let contents = std::fs::read_to_string("/proc/meminfo").expect("Could not read /proc/meminfo");
-    let mut meminfo = HashMap::new();
-    for line in contents.lines().take(3) {
-        let mut split_line = line.split_whitespace();
-        let label = split_line.next();
-        let value = split_line.next();
-        if value.is_some() && label.is_some() {
-            let label = label.unwrap().split(':').nth(0).unwrap();
-            let value = value.unwrap().parse::<u64>().ok().unwrap();
-            meminfo.insert(String::from(label), value/1000000);
-        }
-    }
-    meminfo
-}
-
-fn get_load_avg() -> Vec<f64> {
-    let contents = std::fs::read_to_string("/proc/loadavg").expect("Could not read /proc/loadavg");
-    contents.trim().split(' ')
-        .take(3)
-        .map(|val| val.parse::<f64>().unwrap())
-        .collect::<Vec<f64>>()
+    print!("{} {} ", battery_status, battery_charge_percentage);
 }
 
